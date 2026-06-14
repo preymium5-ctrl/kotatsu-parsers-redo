496d3a58331537c969ac9705076091dfa07d4509
preymium5-ctrl <270125304+preymium5-ctrl@users.noreply.github.com>
2026-06-14 15:21:17 +0800

    Fix MangaDistrict catalog parser

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/MangaDistrict.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/MangaDistrict.kt
@@ -17,6 +17,8 @@ internal class MangaDistrict(context: MangaLoaderContext) :
 	MadaraParser(context, MangaParserSource.MANGA_DISTRICT, "mangadistrict.com", pageSize = 30) {
 
 	override val tagPrefix = "publication-genre/"
+	override val withoutAjax = true
+	override val listUrl = "series/"
 
 	override suspend fun getChapters(manga: Manga, doc: Document): List<MangaChapter> {
 		val slug = manga.url.removeSuffix('/').substringAfterLast('/')
